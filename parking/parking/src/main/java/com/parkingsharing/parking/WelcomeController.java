c1fddf30946673a2cec3bb5593216b86f8146220
SzDudek <simod2002@gmail.com>
2024-02-13 12:26:37 +0100

    initial commit

---
+++ parking/parking/src/main/java/com/parkingsharing/parking/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.parkingsharing.parking;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Spring boot!";
+    }
+}
