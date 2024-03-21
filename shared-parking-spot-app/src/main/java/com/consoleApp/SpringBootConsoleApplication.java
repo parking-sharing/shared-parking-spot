b3cf652e21af733d4e748a9fb7cca4eec9de4935
Dominik <263915@student.pwr.edu.pl>
2024-03-21 13:28:02 +0100

    Add migration run in SpringBootConsoleApplication.java

--- shared-parking-spot-app/src/main/java/com/consoleApp/SpringBootConsoleApplication.java
+++ shared-parking-spot-app/src/main/java/com/consoleApp/SpringBootConsoleApplication.java
@@ -1,5 +1,7 @@
 package com.consoleApp;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
@@ -8,6 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootConsoleApplication implements CommandLineRunner {
+
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dataSourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dataSourcePassword;
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SpringBootConsoleApplication.class);
@@ -18,6 +29,10 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
         System.out.println("Console app is working.");
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSourceUrl, dataSourceUsername, dataSourcePassword)
+                .load();
+        flyway.migrate();
     }
 
 
