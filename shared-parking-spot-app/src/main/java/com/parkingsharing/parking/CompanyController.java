923c90ffe42755f746b6617c11c843c77b9ddeb2
Dominik <263915@student.pwr.edu.pl>
2024-03-21 21:50:07 +0100

    Add CompanyRepository class and change application.yaml, CompanyController class

--- shared-parking-spot-app/src/main/java/com/parkingsharing/parking/CompanyController.java
+++ shared-parking-spot-app/src/main/java/com/parkingsharing/parking/CompanyController.java
@@ -1,48 +1,65 @@
 package com.parkingsharing.parking;
 
+import com.parkingsharing.sql.Company;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    private List<Company> companies = new ArrayList<>(Arrays.asList(new Company(1, "IBM", "example company 1"), new Company(2, "Intel", "example company 2")));
+
+    @Autowired
+    private final CompanyRepository companyRepository;
+
+    public CompanyController(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+
     @GetMapping
     public List<Company> getAllCompanies() {
-        return companies;
+        return companyRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Company getCompanyById(@PathVariable("id") int id) {
-        for(Company company : companies){
-            if(company.id == id){
-                return company;
-            }
-        }
-        return null;
+        return companyRepository.findById(id).orElse(null);
     }
 
-    @PostMapping("")
+    @PostMapping
     public void addCompany(@RequestBody Company company) {
-        companies.add(company);
+        companyRepository.save(company);
     }
 
     @PutMapping("/{id}")
-    public void updateCompany(@PathVariable("id") int id, @RequestBody Company updatedCompany) {
-        Company company = getCompanyById(id);
-        if (company != null) {
-            company.name = updatedCompany.name;
-            company.description = updatedCompany.description;
+    public ResponseEntity updateCompany(@PathVariable int id, @RequestBody Company updatedCompany) {
+        Optional<Company> companyOptional = companyRepository.findById(id);
+        if (!companyOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
         }
+
+        Company existingCompany = companyOptional.get();
+        existingCompany.setName(updatedCompany.getName());
+        existingCompany.setDescription(updatedCompany.getDescription());
+
+        companyRepository.save(existingCompany);
+
+        return ResponseEntity.ok(existingCompany);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCompany(@PathVariable("id") int id) {
-        Company companyToDelete = getCompanyById(id);
-        companies.remove(companyToDelete);
+    public ResponseEntity deleteCompany(@PathVariable int id) {
+        Optional<Company> companyOptional = companyRepository.findById(id);
+        if (!companyOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        companyRepository.deleteById(id);
+
+        return ResponseEntity.ok().build();
     }
 }
 
---
+++ shared-parking-spot-app/src/main/java/com/parkingsharing/parking/CompanyRepository.java
@@ -0,0 +1,8 @@
+package com.parkingsharing.parking;
+import com.parkingsharing.sql.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
+}
