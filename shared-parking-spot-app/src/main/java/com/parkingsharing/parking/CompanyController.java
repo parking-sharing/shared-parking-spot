fbfb4f4c4bec1783a70d611e9ba9338f1572b902
Zbyszek Stasienko <zbyszekstasienko@gmail.com>
2024-03-23 19:59:55 +0100

    add jpa and create new yaml file for console profile

--- shared-parking-spot-app/src/main/java/com/parkingsharing/parking/CompanyController.java
+++ shared-parking-spot-app/src/main/java/com/parkingsharing/parking/CompanyController.java
@@ -1,48 +1,49 @@
 package com.parkingsharing.parking;
 
+import com.parkingsharing.sql.Company;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    private List<Company> companies = new ArrayList<>(Arrays.asList(new Company(1, "IBM", "example company 1"), new Company(2, "Intel", "example company 2")));
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
     @GetMapping
     public List<Company> getAllCompanies() {
-        return companies;
+        return companyRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Company getCompanyById(@PathVariable("id") int id) {
-        for(Company company : companies){
-            if(company.id == id){
-                return company;
-            }
-        }
-        return null;
+        return companyRepository.findById(id).orElse(null);
     }
 
     @PostMapping("")
-    public void addCompany(@RequestBody Company company) {
-        companies.add(company);
+    public Company addCompany(@RequestBody Company company) {
+        return companyRepository.save(company);
     }
 
     @PutMapping("/{id}")
-    public void updateCompany(@PathVariable("id") int id, @RequestBody Company updatedCompany) {
-        Company company = getCompanyById(id);
-        if (company != null) {
-            company.name = updatedCompany.name;
-            company.description = updatedCompany.description;
-        }
+    public Company updateCompany(@PathVariable("id") int id, @RequestBody Company updatedCompany) {
+        return companyRepository.findById(id)
+                .map(company -> {
+                    company.setName(updatedCompany.getName());
+                    company.setDescription(updatedCompany.getDescription());
+                    return companyRepository.save(company);
+                }).orElseGet(() -> {
+                    updatedCompany.setId(id);
+                    return companyRepository.save(updatedCompany);
+                });
     }
 
     @DeleteMapping("/{id}")
     public void deleteCompany(@PathVariable("id") int id) {
-        Company companyToDelete = getCompanyById(id);
-        companies.remove(companyToDelete);
+        companyRepository.deleteById(id);
     }
 }
 
